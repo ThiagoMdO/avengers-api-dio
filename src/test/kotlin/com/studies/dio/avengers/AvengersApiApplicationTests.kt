87543ee75a0d9eb8fa1802287725c622632537ed
ThiagoMdO <thiago_catureba@hotmail.com>
2024-08-23 11:16:45 -0300

    first commit, inicialization project

---
+++ src/test/kotlin/com/studies/dio/avengers/AvengersApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.studies.dio.avengers
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AvengersApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
